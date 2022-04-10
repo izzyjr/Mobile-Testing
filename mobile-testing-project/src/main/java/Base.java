5336c3b72d6695a7c2d93075bb9a27ac0fabd1f9
Israel Mesa <mesa.israel50@gmail.com>
2022-04-10 17:31:47 -0400

    Starting Android App in Emulator

---
+++ mobile-testing-project/src/main/java/Base.java
@@ -0,0 +1,25 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Base {
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        File app = new File("src", "ApiDemos-debug.apk");
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2 XL API 30");
+        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        cap.setCapability(MobileCapabilityType.APPLICATION_NAME, "uiautomator2");
+
+        AndroidDriver driver = new AndroidDriver(url, cap);
+
+    }
+
+}
