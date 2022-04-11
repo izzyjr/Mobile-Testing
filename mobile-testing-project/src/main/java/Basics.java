67712091b6d47839713c471f0e26ad6ce756f129
Israel Mesa <mesa.israel50@gmail.com>
2022-04-11 11:39:58 -0400

    clicking elements

--- mobile-testing-project/src/main/java/Basics.java
+++ mobile-testing-project/src/main/java/Basics.java
@@ -1,11 +1,18 @@
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 public class Basics extends Base {
     public static void main(String[] args) throws MalformedURLException {
 
         AndroidDriver driver = capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Preference']")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@text='3. Preference dependencies']")).click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
     }
 }
