bff1b23a7d670ad83b4316cd31d8599bf028b780
Israel Mesa <mesa.israel50@gmail.com>
2022-04-13 23:16:35 -0400

    convert all locators to use @AndroidFindBy

--- android-testing-project/src/test/java/pages/ApiDemosDebugPage.java
+++ android-testing-project/src/test/java/pages/ApiDemosDebugPage.java
@@ -5,7 +5,6 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 import java.net.MalformedURLException;
@@ -21,12 +20,23 @@ public class ApiDemosDebugPage {
     AndroidDriver<AndroidElement> driver;
     TouchAction touchAction;
 
-    By preference = By.xpath("//android.widget.TextView[@text='Preference']");
-    By preferenceDependencies = By.xpath("//android.widget.TextView[@text='3. Preference dependencies']");
-    By wifiCheckbox = By.id("android:id/checkbox");
-    By wifiSettings = By.xpath("(//android.widget.RelativeLayout)[2]");
-    By editText = By.className("android.widget.EditText");
-    By buttons = By.className("android.widget.Button");
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='Preference']")
+    private AndroidElement preference;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='3. Preference dependencies']")
+    private AndroidElement preferenceDependencies;
+
+    @AndroidFindBy(id = "android:id/checkbox")
+    private AndroidElement wifiCheckbox;
+
+    @AndroidFindBy(xpath = "(//android.widget.RelativeLayout)[2]")
+    private AndroidElement wifiSettings;
+
+    @AndroidFindBy(className = "android.widget.EditText")
+    private AndroidElement editText;
+
+    @AndroidFindBy(className = "android.widget.Button")
+    private List<AndroidElement> buttons;
 
     @AndroidFindBy(uiAutomator = "text(\"Views\")")
     private AndroidElement views;
@@ -73,27 +83,27 @@ public class ApiDemosDebugPage {
     }
 
     public void clickOnPreference() {
-        driver.findElement(preference).click();
+        preference.click();
     }
 
     public void clickOnPreferenceDependencies() {
-        driver.findElement(preferenceDependencies).click();
+        preferenceDependencies.click();
     }
 
     public void clickOnWifiCheckbox() {
-        driver.findElement(wifiCheckbox).click();
+        wifiCheckbox.click();
     }
 
     public void clickOnWifiSettings() {
-        driver.findElement(wifiSettings).click();
+        wifiSettings.click();
     }
 
     public void inputTextInEditText(String text) {
-        driver.findElement(editText).sendKeys(text);
+        editText.sendKeys(text);
     }
 
     public void selectButton(int buttonNumber) {
-        driver.findElements(buttons).get(buttonNumber).click();
+        buttons.get(buttonNumber).click();
     }
 
     public void clickOnViews() {
