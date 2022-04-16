c030c5b536f080119f5879ea7ae68e1a3f9f0ab7
Israel Mesa <mesa.israel50@gmail.com>
2022-04-15 21:17:02 -0400

    products

---
+++ general-store-automation-project/src/test/java/pages/ProductPage.java
@@ -0,0 +1,62 @@
+package pages;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+import java.net.MalformedURLException;
+import java.util.List;
+
+public class ProductPage {
+
+    AndroidDriver<AndroidElement> driver;
+    TouchAction touchAction;
+
+    @AndroidFindBy(uiAutomator = "new UiScrollable(new UiSelector()" +
+            ".resourceId(\"com.androidsample.generalstore:id/rvProductList\"))" +
+            ".scrollIntoView(new UiSelector().textMatches(\"Nike SFB Jungle\")" +
+            ".instance(0))")
+    private AndroidElement scrollToNikeJungle;
+
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/productName")
+    private List<AndroidElement> onscreenProducts;
+
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/productAddCart")
+    private List<AndroidElement> onscreenProductAddToCartButtons;
+
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/counterText")
+    private AndroidElement cartCounter;
+
+    public ProductPage(AndroidDriver<AndroidElement> driver) throws MalformedURLException {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public ProductPage(AndroidDriver<AndroidElement> driver, TouchAction touchAction) throws MalformedURLException {
+        this.driver = driver;
+        this.touchAction = touchAction;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void addNikeJungleToCart() {
+        if (scrollToNikeJungle.isDisplayed()) {
+            findProductAndClickAddToCartButton(onscreenProducts, scrollToNikeJungle.getText());
+        }
+    }
+
+    public boolean isCartCounterAt(String numberOfProducts) {
+        return cartCounter.getText().equals(numberOfProducts);
+    }
+
+    private void findProductAndClickAddToCartButton(List<AndroidElement> list, String productName) {
+        int count = list.size();
+        for (int i = 0; i < count; i++) {
+            if (list.get(i).getText().equals(productName)) {
+                onscreenProductAddToCartButtons.get(i).click();
+            }
+        }
+    }
+}
---
+++ general-store-automation-project/src/test/java/tests/Products.java
@@ -0,0 +1,35 @@
+package tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProductPage;
+
+import java.net.MalformedURLException;
+
+import static org.testng.Assert.assertTrue;
+import static utils.Driver.capabilities;
+
+public class Products {
+
+    private AndroidDriver<AndroidElement> driver;
+    private LoginPage loginPage;
+    private ProductPage productPage;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        driver = capabilities("emulator");
+        loginPage = new LoginPage(driver);
+        productPage = new ProductPage(driver);
+    }
+
+    @Test
+    public void findAndAddProductToCart() {
+        loginPage.goToProductsPage();
+        productPage.addNikeJungleToCart();
+        assertTrue(productPage.isCartCounterAt("1"));
+    }
+
+}
